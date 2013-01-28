48ffbb9e0ae74d936dbcdddcad572386e84c9d73
Thibaud Colas <thibaudcolas@gmail.com>
2013-01-28 14:05:11 +0100

    Add the FMODSoundManager

---
+++ Classes/engine/managers/FmodSoundManager.h
@@ -0,0 +1,82 @@
+//
+//  FmodSoundManager.h
+//  iCopter
+//
+//  Created by disadb on 1/28/13.
+//  Copyright (c) 2013 Patches. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Fmod_event.h"
+#import "fmod_errors.h"
+
+
+#define MAX_FMOD_AUDIO_CHANNELS 10
+// Convenience macro/function for logging FMOD errors
+
+enum
+{
+    gameMusic=0,
+    pauseMusic=1,
+    goSound=2,
+    gameOverSound=3,
+    helicopterSound=4,
+    helicopterShoot=5,
+    helicopterAltitudeAlert=6,
+    helicopterExplosion=7,
+    helicopterMissileDetonates=8,
+    rocketLauncherSound=9,
+    rocketLauncherShoot=10,
+    rocketLauncherExplosion=11,
+    ufo=12,
+    michou=13
+};
+
+@interface FmodSoundManager : NSObject
+{
+    NSMutableArray *sounds;
+
+    //SoundManager *sharedSoundManager;
+	FMOD_EVENTSYSTEM *eventSystem;
+    FMOD_EVENTPARAMETER *nbUfoParam,*nbRocketLauncherParam,*timeParam;// parameter of the event_music_game
+    
+    FMOD_EVENTPROJECT *project;
+    FMOD_EVENTGROUP *generalGrp;
+    FMOD_EVENTGROUP *hGrp;
+    FMOD_EVENTGROUP *rlGrp;
+    FMOD_EVENTGROUP *uGrp;
+    FMOD_EVENT *generalEv;
+    FMOD_EVENT *pauseEv;
+    FMOD_EVENT *gameOverEv;
+    FMOD_EVENT *goEv;
+    FMOD_EVENT *hEv;
+    FMOD_EVENT *hShootEv;
+    FMOD_EVENT *hExplosEv;
+    FMOD_EVENT *hAlertEv;
+    FMOD_EVENT *hMissileDetonEv;
+    FMOD_EVENT *rlEv;
+    FMOD_EVENT *rlExplosEv;
+    FMOD_EVENT *rlShootEv;
+    FMOD_EVENT *uEv;
+    FMOD_EVENT *michouEv;
+}
+
+// Returns an instance of the FmodSoundManager class.  If an instance has already been created
+// then this instance is returned, otherwise a new instance is created and returned.
++ (FmodSoundManager *) sharedFmodSoundManager;
+
+- (id) init;
+- (void) update;
+- (void) add:(int)sound;
+- (void) add:(int)sound immediate:(Boolean)immediate;
+- (FMOD_EVENT *) getEvent:(int)sound;
+- (void) pause:(int)sound;
+- (void) play:(int)sound;
+- (void) stop:(int)sound immediate:(Boolean)immediate;
+- (Boolean) isPaused:(int)sound;
+- (void) initMusicParams;
+- (void) updateMusicParams:(float)timeEllapsed rocketLaunchersCount:(int)rocketLaunchersCount ufoCount:(int)ufoCount;
+- (void) release:(int)sound immediate:(Boolean)immediate;
+- (void) dealloc;
+
+@end
---
+++ Classes/engine/managers/FmodSoundManager.mm
@@ -0,0 +1,228 @@
+//
+//  @public      	CGPoint position;  	Animation *animation; fmodSOundManager.m
+//  iCopter
+//
+//  Created by disadb on 1/28/13.
+//  Copyright (c) 2013 Patches. All rights reserved.
+//
+
+#import "SynthesizeSingleton.h"
+#import "FmodSoundManager.h"
+
+
+
+@implementation FmodSoundManager
+
+// Make this class a singleton class
+SYNTHESIZE_SINGLETON_FOR_CLASS(FmodSoundManager);
+
+#define checkFMODError(e) _checkFMODError(__FILE__, __LINE__, e)
+void _checkFMODError(const char *sourceFile, int line, FMOD_RESULT errorCode);
+// Convenience function for constraining parameter values
+float constrainFloat(float value, float lowerLimit, float upperLimit);
+
+
+- (id) init
+{
+	self= [super init];
+    
+    if (self != nil)
+    {
+        NSLog(@"SOUND INIT");
+        sounds = [[NSMutableArray alloc] init];
+        
+        // Initialize the FMOD event system
+        FMOD_EventSystem_Create(&eventSystem);
+        checkFMODError(FMOD_EventSystem_Init(eventSystem, MAX_FMOD_AUDIO_CHANNELS, FMOD_INIT_NORMAL, NULL, FMOD_EVENT_INIT_NORMAL));
+        // Load the FEV file
+        NSString *fevPath = [[NSBundle mainBundle] pathForResource:@"iCopter" ofType:@"fev"];
+        checkFMODError(FMOD_EventSystem_Load(eventSystem, [fevPath UTF8String], FMOD_EVENT_INIT_NORMAL, &project));
+
+        //charge les groupes d'evenements sonores et les evenements eux-memes
+        //general
+        checkFMODError(FMOD_EventProject_GetGroup(project, "general", 1, &generalGrp));
+        checkFMODError(FMOD_EventGroup_GetEvent(generalGrp, "game_music", FMOD_EVENT_DEFAULT, &generalEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(generalGrp, "pause_music", FMOD_EVENT_DEFAULT, &pauseEv));
+        //checkFMODError(FMOD_EventGroup_GetEvent(generalGrp, "go_sound", FMOD_EVENT_DEFAULT, &pauseEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(generalGrp, "game_over", FMOD_EVENT_DEFAULT, &gameOverEv));
+        checkFMODError(FMOD_EventProject_GetGroup(project, "helicopter", 1, &hGrp));
+        checkFMODError(FMOD_EventGroup_GetEvent(hGrp, "helicopter", FMOD_EVENT_DEFAULT, &hEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(hGrp, "helicopter_altitude_alert", FMOD_EVENT_DEFAULT, &hAlertEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(hGrp, "helicopter_explosion", FMOD_EVENT_DEFAULT, &hExplosEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(hGrp, "helicopter_shoot", FMOD_EVENT_DEFAULT, &hShootEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(hGrp, "helicopter_missile_detonates", FMOD_EVENT_DEFAULT, &hMissileDetonEv));
+        checkFMODError(FMOD_EventProject_GetGroup(project, "rocketLauncher", 1, &rlGrp));
+        checkFMODError(FMOD_EventGroup_GetEvent(rlGrp, "rocketLauncher", FMOD_EVENT_DEFAULT, &rlEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(rlGrp, "rocketLauncher_explosion", FMOD_EVENT_DEFAULT, &rlExplosEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(rlGrp, "rocketLauncher_shoot", FMOD_EVENT_DEFAULT, &rlShootEv));
+        checkFMODError(FMOD_EventProject_GetGroup(project, "ufo", 1, &uGrp));
+        checkFMODError(FMOD_EventGroup_GetEvent(uGrp, "ufo", FMOD_EVENT_DEFAULT, &uEv));
+        checkFMODError(FMOD_EventGroup_GetEvent(uGrp, "michou", FMOD_EVENT_DEFAULT, &michouEv));
+
+        NSLog(@"INFO - Sound: Created successfully");
+    }
+    
+    return self;
+}
+
+- (void) add:(int)sound
+{
+    [self add:sound immediate:true];
+}
+
+- (void) add:(int)sound immediate:(Boolean)immediate
+{
+    checkFMODError(FMOD_Event_Start([self getEvent:sound]));
+    if (!immediate)[self pause:sound];
+}
+
+- (FMOD_EVENT *) getEvent:(int)sound
+{
+    FMOD_EVENT *event;
+    switch (sound)
+    {
+        case gameMusic:
+            event= generalEv;
+            break;
+        case pauseMusic:
+            event= pauseEv;
+            break;
+        case gameOverSound:
+            event= gameOverEv;
+            break;
+        case goSound:
+            event= goEv;
+            break;
+        case helicopterSound:
+            event= hEv;
+            break;
+        case helicopterShoot:
+            event= hShootEv;
+            break;
+        case helicopterExplosion:
+            event= hExplosEv;
+            break;
+        case helicopterAltitudeAlert:
+            event= hAlertEv;
+            break;
+        case helicopterMissileDetonates:
+            event= hMissileDetonEv;
+            break;
+        case rocketLauncherSound:
+            event= rlEv;
+            break;
+        case rocketLauncherExplosion:
+            event= rlExplosEv;
+            break;
+        case rocketLauncherShoot:
+            event= rlShootEv;
+            break;
+        case ufo:
+            event= uEv;
+            break;
+        case michou:
+            event= michouEv;
+            break;
+        default:
+            event= NULL;
+            break;
+    }
+    return event;
+}
+
+- (void) update
+{
+    checkFMODError(FMOD_EventSystem_Update(eventSystem));
+}
+
+- (void) pause:(int)sound
+{
+    checkFMODError(FMOD_Event_SetPaused([self getEvent:sound],1));
+}
+
+- (void) play:(int)sound
+{
+    checkFMODError(FMOD_Event_SetPaused([self getEvent:sound],0));
+}
+
+- (void) stop:(int)sound immediate:(Boolean)immediate
+{
+    checkFMODError(FMOD_Event_Stop([self getEvent:sound],immediate));
+}
+
+- (Boolean) isPaused:(int)sound
+{
+    FMOD_BOOL *paused;
+    checkFMODError(FMOD_Event_GetPaused([self getEvent:sound], paused));
+    return paused?true:false;
+}
+
+- (void) setParameter:(int)sound parameter:param
+{
+}
+
+- (void) initMusicParams
+{
+    //parametres pour la musique dynamique
+    float minTime, maxTime, minRocketLauncher, maxRocketLauncher,minUfo,maxUfo;
+    // get the value of parameter of the event
+    checkFMODError(FMOD_Event_GetParameter(generalEv, "time", &timeParam));
+    checkFMODError(FMOD_Event_GetParameter(generalEv, "rocketLauncher_count", &nbRocketLauncherParam));
+    checkFMODError(FMOD_Event_GetParameter(generalEv, "ufo_count", &nbUfoParam));
+    // get the required range of the parameter and constrain the value
+    checkFMODError(FMOD_EventParameter_GetRange(timeParam, &minTime, &maxTime));
+    checkFMODError(FMOD_EventParameter_GetRange(nbRocketLauncherParam, &minRocketLauncher, &maxRocketLauncher));
+    checkFMODError(FMOD_EventParameter_GetRange(nbUfoParam, &minUfo, &maxUfo));
+
+    // set the new value
+    checkFMODError(FMOD_EventParameter_SetValue(timeParam, constrainFloat(0, minTime, maxTime)));
+    checkFMODError(FMOD_EventParameter_SetValue(nbRocketLauncherParam, constrainFloat(0, minRocketLauncher, maxRocketLauncher)));
+    checkFMODError(FMOD_EventParameter_SetValue(nbUfoParam, constrainFloat(0, minUfo, maxUfo)));
+}
+- (void) updateMusicParams:(float)timeEllapsed rocketLaunchersCount:(int)rocketLaunchersCount ufoCount:(int)ufoCount
+{
+    float minTime, maxTime, minRocketLauncher, maxRocketLauncher,minUfo,maxUfo;
+     // get the value of parameter of the event
+     checkFMODError(FMOD_Event_GetParameter(generalEv, "time", &timeParam));
+     checkFMODError(FMOD_Event_GetParameter(generalEv, "rocketLauncher_count", &nbRocketLauncherParam));
+     checkFMODError(FMOD_Event_GetParameter(generalEv, "ufo_count", &nbUfoParam));
+     // get the required range of the parameter and constrain the value
+     checkFMODError(FMOD_EventParameter_GetRange(timeParam, &minTime, &maxTime));
+     checkFMODError(FMOD_EventParameter_GetRange(nbRocketLauncherParam, &minRocketLauncher, &maxRocketLauncher));
+     checkFMODError(FMOD_EventParameter_GetRange(nbUfoParam, &minUfo, &maxUfo));
+     // set the new value
+     checkFMODError(FMOD_EventParameter_SetValue(timeParam, constrainFloat(timeEllapsed/100, minTime, maxTime)));
+     checkFMODError(FMOD_EventParameter_SetValue(nbRocketLauncherParam, constrainFloat(rocketLaunchersCount, minRocketLauncher,
+                                                                                       maxRocketLauncher)));
+     checkFMODError(FMOD_EventParameter_SetValue(nbUfoParam, constrainFloat(ufoCount, minUfo, maxUfo)));
+}
+
+- (void) release:(int)sound immediate:(Boolean)immediate
+{
+   checkFMODError(FMOD_Event_Release([self getEvent:sound],true,!immediate));
+}
+
+- (void) dealloc
+{
+    NSLog(@"INFO - Explosion: Removed successfully");
+    [super dealloc];
+}
+
+@end
+
+void _checkFMODError(const char *sourceFile, int line, FMOD_RESULT errorCode)
+{
+	if (errorCode != FMOD_OK)
+	{
+		NSString *filename = [[NSString stringWithUTF8String:sourceFile] lastPathComponent];
+		NSLog(@"%@:%d FMOD Error %d:%s", filename, line, errorCode, FMOD_ErrorString(errorCode));
+	}
+}
+
+float constrainFloat(float value, float lowerLimit, float upperLimit)
+{
+	if (value < lowerLimit) value = lowerLimit;
+	if (value > upperLimit) value = upperLimit;
+	
+	return value;
+}
